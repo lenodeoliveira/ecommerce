e51b6b33b31cbb5fdc67515f876e8c4e5516bdaa
Lenodeoliveira <lenodeoliveira@gmail.com>
2019-02-26 20:15:45 -0300

    tpl site

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -17,8 +17,8 @@ class Page {
         $this->options = array_merge($this->defaults, $opts);
 
 	    $config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/ecommerce/views/",
-			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/ecommerce/views-cache/",
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 			"debug"         => false 
 		   );
 
