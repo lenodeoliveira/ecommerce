436b47a44f8615a56e9794b419ca49cd61ddcb4d
Lenodeoliveira <lenodeoliveira@gmail.com>
2019-03-07 20:55:00 -0300

    email de recuperação

--- index.php
+++ index.php
@@ -161,9 +161,61 @@ $app->post("/admin/users/:iduser", function($iduser) {
 
 });
 
+$app->get("/admin/forgot", function(){
 
 
+  $page = new PageAdmin([
+
+     "header"=>false, 
+     "footer"=>false
+
+  ]);
+
+  $page->setTpl("forgot");
+
+});
+
+$app->post("/admin/forgot", function(){
+
+   $user = User::getForgot($_POST["email"]);
+
+   header("Location: /admin/forgot/sent");
+   exit;
+
+});
+
+$app->get("/admin/forgot/sent", function(){
+
+$page = new PageAdmin([
+
+     "header"=>false, 
+     "footer"=>false
+
+  ]);
+
+  	$page->setTpl("forgot-sent");
+
+
+});
+
+$app->get("/admin/forgot/reset", function (){
+
+    /*$user = User::validForgotDecrypt();*/
+
+ 	$page = new PageAdmin([
+
+     "header"=>false, 
+     "footer"=>false
+
+  ]);
+
+  $page->setTpl("forgot-reset");/*, array(
+   "name"=>$user["desperson"],
+   "code"=>$_GET["code"]
+  ));*/
+});
 
 $app->run();
 
- ?>
+
+?>
