8be2383656b2cb1da1e4cbcab530ad3303fbf719
Lenodeoliveira <lenodeoliveira@gmail.com>
2019-03-20 22:41:26 -0300

    Admin-produtos

---
+++ admin-categories.php
@@ -0,0 +1,115 @@
+<?php
+
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+use \Hcode\Model\Category;
+
+$app->get("/admin/categories", function(){
+
+   User::verifyLogin();	
+   
+   $page = new PageAdmin();
+
+   $categories = Category::listAll();
+
+   $page->setTpl("categories", [
+       'categories'=>$categories
+
+   ]);
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+	 User::verifyLogin();
+
+     $page = new PageAdmin();
+
+     $page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+     User::verifyLogin();
+
+     $category = new Category();
+
+     $category->setData($_POST);
+
+     $category->save();
+
+     header("Location: /admin/categories");
+     exit;
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $category->delete();
+
+    header('Location: /admin/categories');
+    exit;
+
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+   User::verifyLogin(); 
+
+   $category = new Category();
+
+   $category->get((int)$idcategory); 
+
+   $page = new PageAdmin();
+   
+   $page->setTpl("categories-update", [
+      'category'=>$category->getValues()
+  
+   ]);
+
+});
+
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+   User::verifyLogin();
+
+   $category = new Category();
+
+   $category->get((int)$idcategory); 
+
+   $category->setData($_POST);
+
+   $category->save();
+
+   header('Location: /admin/categories');
+   exit;
+   
+
+});
+
+$app->get("/categories/:idcategory", function ($idcategory){
+   
+   $page = new 	Page();
+
+   $category = new Category();
+ 
+   $category->get((int)$idcategory);
+
+   $page->setTpl("category", [
+       'category'=>$category->getValues(),
+       'products'=>[]
+
+   ]);  
+
+});
+
+
+?>
